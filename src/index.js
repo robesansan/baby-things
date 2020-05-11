b5784c7a154bcc1894c19f62b29a4418a115a727
robertono <roberto.san.san@gmail.com>
2020-05-11 13:40:44 +0200

    chore(transpile): add ES6 support to code and transpile in dist folder
    add ES6 support to code and transpile in dist folder (not commited), build script working but test
    script not until next commit (add ES6 support to tests)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-const uniqueRandomArray = require('unique-random-array');
-const babyThings = require('./baby-things.json')
+import uniqueRandomArray from 'unique-random-array'
+import babyThings from './baby-things.json'
 
 const mainExport = {
     all: babyThings,
     random: uniqueRandomArray(babyThings),
 }
 
+export default mainExport
 module.exports = mainExport // for CommonJS compatibility
-
-//import uniqueRandomArray from 'unique-random-array'
-//import babyThings from './baby-things.json'
-//
-//const mainExport = {
-//    all: babyThings,
-//    random: uniqueRandomArray(babyThings),
-//}
-//
-//export default mainExport
-//module.exports = mainExport // for CommonJS compatibility
